08f9233e755f795a7340d3eee157bd47ce22cf40
Gustavobvs1 <gustavobvs06@gmail.com>
2024-04-02 20:26:10 -0300

    add uncompleted project pass-in

---
+++ Desafios/pass-in/src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -0,0 +1,41 @@
+package rocketseat.com.passin.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import rocketseat.com.passin.dto.attendee.AttendeesListResponseDTO;
+import rocketseat.com.passin.dto.event.EventIdDTO;
+import rocketseat.com.passin.dto.event.EventRequestDTO;
+import rocketseat.com.passin.dto.event.EventResponseDTO;
+import rocketseat.com.passin.services.AttendeeService;
+import rocketseat.com.passin.services.EventService;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+    private final AttendeeService attendeeService;
+    @GetMapping("/{eventId}")
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId){
+        EventResponseDTO event =  this.eventService.getEventDetails(eventId);
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+
+        return  ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @GetMapping("/attendees/{eventId}")
+    public ResponseEntity<AttendeesListResponseDTO> getEventAttendees(@PathVariable String eventId){
+        AttendeesListResponseDTO attendeesListResponse =  this.attendeeService.getEventsAttendee(eventId);
+        return ResponseEntity.ok(attendeesListResponse);
+    }
+}
