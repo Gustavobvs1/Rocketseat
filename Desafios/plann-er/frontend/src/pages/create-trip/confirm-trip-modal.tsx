950427adf870ea0ddd651d61d303b8ee35cf27e9
Gustavobvs1 <gustavobvs06@gmail.com>
2024-07-12 20:27:01 -0300

    plann.er frontend almost completed

---
+++ Desafios/plann-er/frontend/src/pages/create-trip/confirm-trip-modal.tsx
@@ -0,0 +1,71 @@
+import { X, User, Mail } from "lucide-react";
+import { FormEvent } from "react";
+import { Button } from "../../components/button";
+
+interface ConfirmTripModalProps {
+  closeConfirmTripModal: () => void;
+  createTrip: (event: FormEvent<HTMLFormElement>) => void;
+  setOwnerName: (name: string) => void;
+  setOwnerEmail: (email: string) => void;
+}
+
+export function ConfirmTripModal({
+  closeConfirmTripModal,
+  createTrip,
+  setOwnerName,
+  setOwnerEmail,
+}: ConfirmTripModalProps) {
+  return (
+    <div className="inset-0 fixed bg-black/60 flex items-center justify-center">
+      <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+        <header className="space-y-2">
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-semibold">
+              Confirmar criação de viagem
+            </h2>
+            <button onClick={closeConfirmTripModal}>
+              <X className="size-5 text-zinc-400" />
+            </button>
+          </div>
+          <p className="text-sm text-zinc-400">
+            Para concluir a criação da viagem para{" "}
+            <span className="font-semibold text-zinc-100">Florianópolis</span> ,
+            Brasil nas datas de{" "}
+            <span className="font-semibold text-zinc-100">
+              16 a 27 de Agosto de 2024
+            </span>{" "}
+            preencha seus dados abaixo:
+          </p>
+        </header>
+
+        <form className="space-y-3" onSubmit={createTrip}>
+          <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <User className="text-zinc-400 size-5" />
+            <input
+              type="text"
+              name="text"
+              placeholder="Seu nome completo"
+              className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"
+              onChange={(event) => setOwnerName(event.target.value)}
+            />
+          </div>
+
+          <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <Mail className="text-zinc-400 size-5" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Seu e-mail pessoal"
+              className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none flex-1"
+              onChange={(event) => setOwnerEmail(event.target.value)}
+            />
+          </div>
+
+          <Button variant="primary" size="full" type="submit">
+            Confirmar criação de viagem
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ Desafios/plann-er/frontend/src/pages/trip-details/guests.tsx
@@ -0,0 +1,56 @@
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Button } from "../../components/button";
+import { api } from "../../lib/axios";
+
+interface ParticipantsProps {
+  id: string;
+  name: string | null;
+  is_confirmed: boolean;
+  email: string;
+}
+
+export function Guests() {
+  const { tripId } = useParams();
+  const [participants, setParticipants] = useState<ParticipantsProps[]>([]);
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/participants`).then((response) => {
+      setParticipants(response.data.participants);
+    });
+  }, [tripId]);
+
+  return (
+    <div className="space-y-6">
+      <h2 className="font-semibold text-xl">Convidados</h2>
+
+      {participants.map((participant, index) => {
+        return (
+          <div className="space-y-5" key={participant.id}>
+            <div className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5">
+                <span className="block font-medium text-zinc-100">
+                  {participant.name || `Convidado ${index}`}
+                </span>
+                <span className="block text-sm text-zinc-400 truncate ">
+                  {participant.email}
+                </span>
+              </div>
+              {participant.is_confirmed ? (
+                <CheckCircle2 className="size-5 text-lime-300 shrink-0" />
+              ) : (
+                <CircleDashed className="size-5 text-zinc-400 shrink-0" />
+              )}
+            </div>
+          </div>
+        );
+      })}
+
+      <Button variant="secondary" size="full">
+        <UserCog className="size-5" />
+        Gerenciar convidados
+      </Button>
+    </div>
+  );
+}
